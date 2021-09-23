50d4c109906043f991c5c9874bb26c87787fc91c
Frank Cooke <frankcooke79@gmail.com>
2021-09-23 15:28:30 -0400

    feat: adds ability to parse multiple cookies by name

--- test/getCookieValue.test.ts
+++ test/getCookieValue.test.ts
@@ -7,13 +7,39 @@ describe('getCookieValue', () => {
       res: {},
       req: {
         headers: {
-          cookie: 'token=1234;foo=bar;',
+          cookie: 'token=1234;foo=bar;cool=story;',
         },
       },
     } as GetServerSidePropsContext;
 
     const cookies = getCookieValue(ctx, 'token');
     expect(cookies).toEqual('1234');
+  });
+  it('should return undefined if cookie not found', () => {
+    const ctx = {
+      res: {},
+      req: {
+        headers: {
+          cookie: 'token=1234;foo=bar;cool=story;',
+        },
+      },
+    } as GetServerSidePropsContext;
+
+    const cookies = getCookieValue(ctx, 'story');
+    expect(cookies).toEqual(undefined);
+  });
+  it('should return an object of values if passed an array of cookie names', () => {
+    const ctx = {
+      res: {},
+      req: {
+        headers: {
+          cookie: 'token=1234;foo=bar;cool=story;',
+        },
+      },
+    } as GetServerSidePropsContext;
+
+    const cookies = getCookieValue(ctx, ['token', 'cool']);
+    expect(cookies).toEqual({ token: '1234', cool: 'story' });
   });
   it('should get a cookies value on the client', () => {
     Object.defineProperty(document, 'cookie', {
